29b62b9f656a73218462e3e8d704d748efa0ac78
eddygulled <edgargulay69@gmail.com>
2023-11-23 20:12:27 +0300

    web inference

---
+++ src/index.js
@@ -0,0 +1,17 @@
+async function query(filename) {
+	const data = fs.readFileSync(filename);
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/eddiegulay/Whisperer_Mozilla_Sw_2000",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: data,
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+query("sample1.flac").then((response) => {
+	console.log(JSON.stringify(response));
+});
---
+++ src/web_infer.js
@@ -0,0 +1,46 @@
+async function query(filename) {
+    const data = new FormData();
+    data.append('file', filename);
+
+    try {
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/eddiegulay/Whisperer_Mozilla_Sw_2000",
+            {
+                method: "POST",
+                body: data,
+                headers: {
+                    Authorization: "Bearer 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",
+                },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error('Error:', error);
+        return { error: 'Failed to process audio.' };
+    }
+}
+
+async function processAudio() {
+    const inputElement = document.getElementById('audioFileInput');
+    const outputElement = document.getElementById('output');
+
+    if (!inputElement.files.length) {
+        outputElement.textContent = 'Please select an audio file.';
+        return;
+    }
+
+    const audioFile = inputElement.files[0];
+
+    try {
+        const result = await query(audioFile);
+        outputElement.textContent = JSON.stringify(result, null, 2);
+    } catch (error) {
+        outputElement.textContent = `Error: ${error.message}`;
+    }
+}
